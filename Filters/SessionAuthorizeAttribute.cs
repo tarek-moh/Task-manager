508acfc16a9ab79aab2b7efb29f723eaff93307b
tarek mohamed <tarekmoh5557@gmail.com>
2024-10-01 16:37:00 +0300

    initial commit

---
+++ Filters/SessionAuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class SessionAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // Access the current session
+        var session = context.HttpContext.Session;
+
+        // Check if session is available and has "AuthorizationObject"
+        if (session == null || string.IsNullOrEmpty(session.GetString("authenticated")))
+        {
+            context.Result = new RedirectToActionResult("Login", "Accounts", null); // Redirect to Unauthorized page
+            return;
+        }
+
+        // Validate if "AuthorizationObject" is equal to "valid"
+        var authorizationObject = session.GetString("authenticated");
+        if (authorizationObject != "valid")
+        {
+            context.Result = new RedirectToActionResult("Login", "Accounts", null); // Redirect to Unauthorized page
+            return;
+        }
+    }
+}
---
+++ Models/account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_Manager.Models
+{
+    public class account
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+
+    }
+}
